e71d16c52effe8a572ad019f6301486702af66f4
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-09-04 00:33:55 +0300

    dirst working application

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Web3 from 'web3';
+import Comp from './Comp';
+
+
+// React.useEffect(() => {
+//   loadBlockchainData;
+// });
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Comp />
+      hey
+    </div>
+  );
+}
+
+export default App;
